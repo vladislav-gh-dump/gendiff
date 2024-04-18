1dc95602209180d086d61f7b656bbfac616c56c5
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-04-18 18:27:14 +0300

    add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import stylisticJs from "@stylistic/eslint-plugin-js"
+
+export default [
+  {
+    plugins: {
+      "@stylistic/js": stylisticJs
+    },
+    rules: {
+      "@stylistic/js/indent": ["error", 2],
+      "no-console": "off",
+    }
+  }
+]
