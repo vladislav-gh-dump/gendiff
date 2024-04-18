5f95b7b52179d60302e738a584da5c0502c9f68e
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-04-18 18:28:48 +0300

    mod gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -5,15 +5,15 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-    .name("gendiff")
-    .description("Compares two configuration files and shows a difference.")
-    .version("1.0.0");
+  .name("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .version("1.0.0");
 
 program
-    .argument("<filepath1>")
-    .argument("<filepath2>");
+  .argument("<filepath1>")
+  .argument("<filepath2>");
 
 program
-    .option("-f, --format [type]", "output format");
+  .option("-f, --format [type]", "output format");
 
 program.parse();
