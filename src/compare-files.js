4efd9dcdce6f84fcbda74f628c94c18b8d669efe
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-04-20 22:07:49 +0300

    add compare-files.js

---
+++ src/compare-files.js
@@ -0,0 +1,33 @@
+export default (object1, object2) => {
+  const commonObject = { ...object1, ...object2 };
+  const sortedKeys = Object.keys(commonObject).sort();
+
+  let output = "";
+  let line = "";
+
+  sortedKeys.forEach((key) => {
+    const hasKeyObject1 = Object.hasOwn(object1, key);
+    const hasKeyObject2 = Object.hasOwn(object2, key);
+
+    const value1 = hasKeyObject1 && object1[key];
+    const value2 = hasKeyObject2 && object2[key]; 
+
+    if (hasKeyObject1 === hasKeyObject2 !== false) {
+      line = `    ${key}: ${value1}\n`;
+
+      if (!(value1 === value2)) {
+        line = `  - ${key}: ${value1}\n  + ${key}: ${value2}\n`;
+      }
+    } else {
+      line = `  - ${key}: ${value1}\n`;
+
+      if (!hasKeyObject1) {
+        line = `  + ${key}: ${value2}\n`;
+      }
+    }
+
+    output += line;
+  });
+
+  return `{\n${output}}`;
+};
