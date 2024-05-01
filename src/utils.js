27d6e769e338e29e439ad3f2e71c18380cf56871
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-05-01 21:15:41 +0300

    edit readFile

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,11 @@ import path from "path";
 
 const composeAbsFilepath = (filepath) => path.resolve(process.cwd(), filepath);
 
-const readFile = (filepath) => fs.readFileSync(composeAbsFilepath(filepath));
+const readFile = (filepath, encoding = "utf8") => {
+  const absFilepath = composeAbsFilepath(filepath);
+  return fs.readFileSync(absFilepath, encoding);
+};
+
 const getFileExt = (filepath) => path.extname(filepath).split(".")[1];
 
 export { readFile, getFileExt };
