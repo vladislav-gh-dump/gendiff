3f66c51b07136a78d356989f4ec0eeb562a564ed
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-05-01 22:21:59 +0300

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,39 @@
 import path from "path";
-import { readFile } from "../src/utils.js";
+import { composeAbsFilepath, readFile } from "../src/utils.js";
+import { getFormatter } from "../src/formatters/index.js";
+import { getParser } from "../src/parsers.js";
 import compareFiles from "../src/index.js";
 
-const __dirname = path.join(process.cwd(), "__tests__");
+
+const __dirname = composeAbsFilepath("__tests__");
 const getFixturePath = (filename) => path.join(__dirname, "..", "__fixtures__", filename);
+
+
+const formats = [ "stylish", "plain", "json" ];
+
+describe("get formatter", () => {
+  test.each(formats)("%s", (format) => {
+    expect(getFormatter(format) && true).toBeTruthy();
+  });
+  test("wrong format", () => {
+    expect(() => getFormatter("wrong")).toThrow();
+  });
+});
+
+describe("get parser", () => {
+  test
+    .each([ 
+      "json", 
+      "yaml", 
+      "yml" 
+    ])("for %s", (ext) => {
+      expect(getParser(ext) && true).toBeTruthy();
+    });
+  
+  test("wrong extension", () => {
+    expect(() => getParser("wrong")).toThrow();
+  });
+});
 
 
 const exts = [ 
@@ -11,7 +41,6 @@ const exts = [
   [ "yaml", "yaml" ],
   [ "json", "yaml" ], 
 ];
-const formats = [ "stylish", "plain", "json" ];
 
 describe
   .each(exts)("gendiff filepath1.%s filepath2.%s", (ext1, ext2) => {
