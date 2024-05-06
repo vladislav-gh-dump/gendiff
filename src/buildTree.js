ccbebcfa2e9fd63a2b4d117c9d0f9e0f465d93f9
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-05-06 16:49:23 +0300

    refactoring

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,32 +1,44 @@
 import _ from "lodash";
 
-const buildItem = (stat, key, value) => ({ stat, key, value });
-
 const getItem = (object1, object2, key) => {
   if (!(_.has(object1, key))) {
-    const value = object2[key];
-    return buildItem("received", key, value);
+    return {
+      stat: "received",
+      key,
+      value: object2[key]
+    }
   }
 
   if (!(_.has(object2, key))) {
-    const value = object1[key];
-    return buildItem("expected", key, value);
+    return {
+      stat: "expected",
+      key,
+      value: object1[key]
+    }
   }
 
-  const value1 = object1[key];
-  const value2 = object2[key];
-
-  if (_.isObject(value1) && _.isObject(value2)) {
-    const tree = buildTree(value1, value2);
-    return buildItem("nested", key, tree);
+  if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+    const tree = buildTree(object1[key], object2[key]);
+    return {
+      stat: "nested",
+      key,
+      value: tree
+    }
   }
 
-  if (value1 === value2) {
-    return buildItem("matched", key, value1);
+  if (object1[key] === object2[key]) {
+    return {
+      stat: "matched",
+      key,
+      value: object1[key]
+    }
   }
 
-  const value = [ value1, value2 ];
-  return buildItem("exchanged", key, value);
+  return {
+    stat: "exchanged",
+    key,
+    value: [ object1[key], object2[key] ]
+  }
 }
 
 const buildTree = (object1, object2) => {
