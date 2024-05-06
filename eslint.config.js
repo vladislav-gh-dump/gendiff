e158e04485e4bfba9305aee99abbcad421d48348
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-05-06 19:25:45 +0300

    set lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,52 @@
-import stylisticJs from "@stylistic/eslint-plugin-js"
+import globals from "globals";
+
+import { composeAbsFilepath } from "./src/utils.js";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+
+const __dirname = composeAbsFilepath(".");
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
   {
-    plugins: {
-      "@stylistic/js": stylisticJs
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
     },
+    plugins: { import: importPlugin },
     rules: {
-      "@stylistic/js/indent": ["error", 2, { "SwitchCase": 1 }]
-    }
-  }
-]
+      ...importPlugin.configs.recommended.rules,
+    },
+  },
+  ...compat.extends("airbnb-base"),
+  {
+    rules: {
+      "no-underscore-dangle": [
+        "error",
+        {
+          allow: ["__filename", "__dirname"],
+        },
+      ],
+      "import/extensions": [
+        "error",
+        {
+          js: "always",
+        },
+      ],
+      "import/no-named-as-default": "off",
+      "import/no-named-as-default-member": "off",
+      "no-console": "off",
+      "import/no-extraneous-dependencies": "off",
+    },
+  },
+];
